cda4fa4842aa5c564bdcf2cc824f5680f848b0c1
Christian Pardo <chrispardopersonal@gmail.com>
2020-03-31 03:00:08 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -15,7 +15,7 @@ def age_appropriate_birthday(birthday_kids)
     if age <= 12
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     else
-    "You are older than 12"
+    puts "You are older than 12"
     end
   end
 end 
