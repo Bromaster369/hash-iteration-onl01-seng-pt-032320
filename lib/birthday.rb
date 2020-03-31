fa5432387a4210e15c047a996399a9da1595d3e4
Christian Pardo <chrispardopersonal@gmail.com>
2020-03-31 02:54:54 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,6 +10,9 @@ def happy_birthday(birthday_kids)
   end
 end
 
-def age_appropriate_birthday()
-
+def age_appropriate_birthday(birthday_kids)
+  if birthday_kids <= 12 do |kids_name, age|
+    puts puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end
 #"only prints the birthday greeting if the birthday kid 12 or younger"
